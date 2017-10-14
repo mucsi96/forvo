84ea78e4b0125644f06e703a44ccd1295d0ceb37
Igor Mucsicska <mucsi_96@yahoo.com>
2017-10-14 18:59:35 +0200

    Improve API coverage

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -4,8 +4,12 @@ import forvoApi from '..';
 jest.mock('../rest');
 
 describe('Forvo API', () => {
+  afterEach(() => {
+    rest.__resetMocks();
+  })
+
   describe('wordPronunciations', () => {
-    it('Returns word pronunciations', async () => {
+    it('Gets all the pronunciations from a word', async () => {
       const expectedWordPronunciations = ['apple1', 'apple2'];
       rest.__setMockResponse({
         method: 'GET',
@@ -14,6 +18,110 @@ describe('Forvo API', () => {
       });
       const forvo = forvoApi({ key: 'test-api-key' });
       const actualWordPronunciations = await forvo.wordPronunciations({ word: 'apple', otherParam: 1 });
+      expect(actualWordPronunciations).toEqual(expectedWordPronunciations);
+    });
+
+    it('Throws error if options is not an object', async () => {
+      expect.assertions(1);
+      const forvo = forvoApi({ key: 'test-api-key' });
+      try {
+        await forvo.wordPronunciations('sadas');
+      } catch(err) {
+        expect(err.message).toEqual('Parameters should be an object');
+      }
+    });
+
+    it('Throws error if word is not provided', async () => {
+      expect.assertions(1);
+      const forvo = forvoApi({ key: 'test-api-key' });
+      try {
+        await forvo.wordPronunciations({});
+      } catch(err) {
+        expect(err.message).toEqual('word is a required parameter');
+      }
+    });
+  });
+
+  describe('standardPronunciation', () => {
+    it('Gets the standard (top rated) pronunciation from a word', async () => {
+      const expectedWordPronunciation = 'apple1';
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/standard-pronunciation/word/apple/other-param/1',
+        body: expectedWordPronunciation
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualWordPronunciation = await forvo.standardPronunciation({ word: 'apple', otherParam: 1 });
+      expect(actualWordPronunciation).toEqual(expectedWordPronunciation);
+    });
+  });
+
+  describe('languageList', () => {
+    it('Gets languages availables at Forvo', async () => {
+      const expectedLanguages = ['en', 'de'];
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/language-list/other-param/1',
+        body: expectedLanguages
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualLanguages = await forvo.languageList({ otherParam: 1 });
+      expect(actualLanguages).toEqual(expectedLanguages);
+    });
+  });
+
+  describe('popularLanguages', () => {
+    it('Gets the most popular languages', async () => {
+      const expectedLanguages = ['en', 'de'];
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/language-popular/other-param/1',
+        body: expectedLanguages
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualLanguages = await forvo.popularLanguages({ otherParam: 1 });
+      expect(actualLanguages).toEqual(expectedLanguages);
+    });
+  });
+
+  describe('pronouncedWordsSearch', () => {
+    it('Gets words starting with a pattern alphabetically ordered with one or more pronunciations', async () => {
+      const expectedWordPronunciations = ['apple1', 'apple2'];
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/pronounced-words-search/search/apple/other-param/1',
+        body: expectedWordPronunciations
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualWordPronunciations = await forvo.pronouncedWordsSearch({ search: 'apple', otherParam: 1 });
+      expect(actualWordPronunciations).toEqual(expectedWordPronunciations);
+    });
+  });
+
+  describe('wordsSearch', () => {
+    it('Gets words starting with a pattern alphabetically ordered', async () => {
+      const expectedWordPronunciations = ['apple1', 'apple2'];
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/words-search/search/apple/other-param/1',
+        body: expectedWordPronunciations
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualWordPronunciations = await forvo.wordsSearch({ search: 'apple', otherParam: 1 });
+      expect(actualWordPronunciations).toEqual(expectedWordPronunciations);
+    });
+  });
+
+  describe('popularPronouncedWords', () => {
+    it('Gets the most popular words with, at least, one pronunciation', async () => {
+      const expectedWordPronunciations = ['apple1', 'apple2'];
+      rest.__setMockResponse({
+        method: 'GET',
+        url: 'https://apifree.forvo.com/key/test-api-key/format/json/action/popular-pronounced-words/other-param/1',
+        body: expectedWordPronunciations
+      });
+      const forvo = forvoApi({ key: 'test-api-key' });
+      const actualWordPronunciations = await forvo.popularPronouncedWords({ otherParam: 1 });
       expect(actualWordPronunciations).toEqual(expectedWordPronunciations);
     });
   });
--- src/index.js
+++ src/index.js
@@ -1,30 +1,197 @@
 import rest from './rest';
 
+const checkParameters = (requiredParams, params) => {
+  if (typeof params !== 'undefined' && typeof params !== 'object') {
+    throw new Error('Parameters should be an object');
+  }
+  requiredParams.forEach(requiredParam => {
+    if (!params || !params[requiredParam]) {
+      throw new Error(`${requiredParam} is a required parameter`);
+    }
+  })
+}
+
 function camelCaseToDash( myStr ) {
   return myStr.replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase();
 }
 
-const sendForvoRequest = ({ key, action, requiredParams, optionalParams }) => {
+const sendForvoRequest = ({ key, action, requiredParams, params }) => {
+  checkParameters(requiredParams, params);
+  const optionalParams = Object.keys(params).filter(name => !requiredParams.includes(name));
   const url = [
     { name: 'key', value: key },
     { name: 'format', value: 'json' },
     { name: 'action', value: action },
-    ...requiredParams,
-    ...Object.keys(optionalParams).map(key => ({ name: key, value: optionalParams[key] }))
+    ...requiredParams.map(name => ({ name, value: params[name] })),
+    ...optionalParams.map(name => ({ name, value: params[name] }))
   ].reduce((url, { name, value }) => {
     return [url, camelCaseToDash(name), value].join('/');
   }, 'https://apifree.forvo.com');
   return rest.GET(url);
 };
 
+/**
+ * This function creates Forgo api.
+ * @param {object} options - Configuration object for Forgo api
+ * @param {string} options.key - Your Forgo api key
+ * @returns {API} Api
+ * @example
+ * import forgoApi from 'forgo';
+ *
+ * const forgo = forgoApi({ key: 'your api key' });
+ */
 const forvoApi = ({ key }) => {
+  /**
+   * This object represents a Forgo Api.
+   * @typedef {Object} Api
+   * @property {Api.wordPronunciations} wordPronunciations - This function gets all the pronunciations from a word.
+   * @property {Api.standardPronunciation} standardPronunciation - This function gets the standard (top rated) pronunciation from a word.
+   * @property {Api.languageList} languageList - This function gets languages availables at Forvo.
+   * @property {Api.popularLanguages} popularLanguages - This function gets the most popular languages.
+   * @property {Api.pronouncedWordsSearch} pronouncedWordsSearch - This function gets words starting with a pattern alphabetically ordered with one or more pronunciations.
+   * @property {Api.wordsSearch} wordsSearch - This function gets words starting with a pattern alphabetically ordered.
+   * @property {Api.popularPronouncedWords} popularPronouncedWords - This function gets the most popular words with, at least, one pronunciation..
+   */
   return {
-    wordPronunciations: ({ word, ...options}) => sendForvoRequest({
+    /**
+     * This function gets all the pronunciations from a word.
+     * @name Api.wordPronunciations
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} parameters.word - The word you want to get its pronunciations.
+     * @param {string} [parameters.language] - To get only the pronunciations recorded in the given language.
+     * @param {string} [parameters.country] - To get only the pronunciations recorded by users of this country. You should use the Alpha-3 code.
+     * @param {string} [parameters.username] - Returns the pronunciation recorded by a specific user.
+     * @param {string} [parameters.sex] - m (for male), f (for female)
+     * @param {number} [parameters.rate] - To get only the pronunciations rated with at least the given value.
+     * @param {string} [parameters.order] - date-desc (pronunciations order by pronounced time, from recent to older)
+        date-asc (pronunciations order by pronounced time, older to recent)
+        rate-desc (pronunciations order by rate, high rated first)
+        rate-asc (pronunciations order by rate, low rated first)
+     * @param {number} [parameters.limit] - Max. pronunciations returned.
+     * @param {boolean} [parameters.groupInLanguages] - Group pronunciations in languages. Default value is false.
+     * @returns {Promise<Object>} - All the pronunciations from a word
+     * @see {@link https://api.forvo.com/documentation/word-pronunciations|Forgo API documentation}
+     * @example
+     * import forgoApi from 'forgo';
+     *
+     * const forgo = forgoApi({ key: 'your api key' });
+     * const wordPronunciations = await forgo.wordPronunciations({ word: 'Apfel', language: 'de' })
+     * // {
+     * //   attributes: { total: 11 },
+     * //   items:[
+     * //     { id: 5943, word: 'apfel', original: 'Apfel', pathmp3: 'https://apifree.forvo.com/audio/3h3h...
+     * //   ]
+     * // }
+     */
+    wordPronunciations: params => sendForvoRequest({
       key,
       action: 'word-pronunciations',
-      requiredParams: [{ name: 'word', value: word }],
-      optionalParams: options
-    })
+      requiredParams: ['word'],
+      params
+    }),
+    /**
+     * This function gets the standard (top rated) pronunciation from a word.
+     * @name Api.standardPronunciation
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} parameters.word - The word you want to get its pronunciations.
+     * @param {string} [parameters.language] - To get only the pronunciations recorded in the given language.
+     * @returns {Promise<Object>} - Standard (top rated) pronunciation from a word
+     * @see {@link https://api.forvo.com/documentation/standard-pronunciation|Forgo API documentation}
+     */
+    standardPronunciation: params => sendForvoRequest({
+      key,
+      action: 'standard-pronunciation',
+      requiredParams: ['word'],
+      params
+    }),
+    /**
+     * This function gets languages availables at Forvo.
+     * @name Api.languageList
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} [parameters.language] - Language code or "native" to get the language list with the names of the languages in their native languages. Default is English (en).
+     * @param {string} [parameters.order] - Values: "name" or "code". Default is name.
+     * @param {number} [parameters.minPronunciations] - Values: any integer number. To get only the languagues with, at least, the given number of pronunciations.
+     * @returns {Promise<Object>} - Languages availables at Forvo.
+     * @see {@link https://api.forvo.com/documentation/language-list|Forgo API documentation}
+     */
+    languageList: params => sendForvoRequest({
+      key,
+      action: 'language-list',
+      requiredParams: [],
+      params
+    }),
+    /**
+     * This function gets the most popular languages.
+     * @name Api.popularLanguages
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} [parameters.language] - Values: Language code or "native" to get the language list with the names of the languages in their native languages. Default is English (en).
+     * @param {string} [parameters.order] - Values: "popular", "name" or "code". Default is "popular".
+     * @param {string} [parameters.limit] - Values: any integer number. Max. languages returned. Default is 10.
+     * @returns {Promise<Object>} - The most popular languages
+     * @see {@link https://api.forvo.com/documentation/language-popular|Forgo API documentation}
+     */
+    popularLanguages: params => sendForvoRequest({
+      key,
+      action: 'language-popular',
+      requiredParams: [],
+      params
+    }),
+    /**
+     * This function gets words starting with a pattern alphabetically ordered with one or more pronunciations.
+     * @name Api.pronouncedWordsSearch
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} parameters.search - The pattern you want to search.
+     * @param {string} [parameters.language] - To get only the pronunciations recorded in the given language.
+     * @param {number} [parameters.pagesize] - Values: any integer number between 1-100. Set the page size in results. Default is 20.
+     * @param {number} [parameters.page] - Values: any integer number. Set the page results you want to retrieve. Default is 1.
+     * @returns {Promise<Object>} - Words starting with a pattern alphabetically ordered with one or more pronunciations
+     * @see {@link https://api.forvo.com/documentation/pronounced-words-search|Forgo API documentation}
+     */
+    pronouncedWordsSearch: params => sendForvoRequest({
+      key,
+      action: 'pronounced-words-search',
+      requiredParams: ['search'],
+      params
+    }),
+    /**
+     * This function gets words starting with a pattern alphabetically ordered.
+     * @name Api.wordsSearch
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} parameters.search - The pattern you want to search.
+     * @param {string} [parameters.language] - To get only the pronunciations recorded in the given language.
+     * @param {number} [parameters.pagesize] - Values: any integer number between 1-100. Set the page size in results. Default is 20.
+     * @param {number} [parameters.page] - Values: any integer number. Set the page results you want to retrieve. Default is 1.
+     * @returns {Promise<Object>} - Words starting with a pattern alphabetically ordered
+     * @see {@link https://api.forvo.com/documentation/words-search|Forgo API documentation}
+     */
+    wordsSearch: params => sendForvoRequest({
+      key,
+      action: 'words-search',
+      requiredParams: ['search'],
+      params
+    }),
+    /**
+     * This function gets the most popular words with, at least, one pronunciation.
+     * @name Api.popularPronouncedWords
+     * @function
+     * @param {object} parameters - Parameters object provided for Forgo api
+     * @param {string} [parameters.language] - To get only the words in the given language.
+     * @param {number} [parameters.limit] - Values: any integer number. Max. word returned. Default is 1000.
+     * @returns {Promise<Object>} - The most popular words with, at least, one pronunciation.
+     * @see {@link https://api.forvo.com/documentation/popular-pronounced-words|Forgo API documentation}
+     */
+    popularPronouncedWords: params => sendForvoRequest({
+      key,
+      action: 'popular-pronounced-words',
+      requiredParams: [],
+      params
+    }),
   }
 }
 
